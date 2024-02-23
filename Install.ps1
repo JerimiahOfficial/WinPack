9044be98978599685cd9c49ed9ee4ae4522b2905
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2024-02-23 00:00:49 +0000

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,18 +1,17 @@
-# Variables
-$Steam      = "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe"
-$Discord    = "https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x64"
-$LibreWolf  = "https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/122.0.1-2/librewolf-122.0.1-2-windows-x86_64-setup.exe"
-$PortMaster = "https://updates.safing.io/latest/windows_amd64/packages/portmaster-installer.exe"
-$Nvidia     = "https://us.download.nvidia.com/nvapp/client/10.0.0.499/NVIDIA_app_beta_v10.0.0.499.exe"
-$Corsair    = "https://www3.corsair.com/software/CUE_V5/public/modules/windows/installer/Install%20iCUE.exe"
-$links = @($Steam, $Discord, $LibreWolf, $PortMaster, $Nvidia, $Corsair)
+$links = @(
+    "https://vault.bitwarden.com/download/?app=desktop&platform=windows",
+    "https://www3.corsair.com/software/CUE_V5/public/modules/windows/installer/Install%20iCUE.exe",
+    "https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x64",
+    "https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/122.0.1-2/librewolf-122.0.1-2-windows-x86_64-setup.exe",
+    "https://us.download.nvidia.com/nvapp/client/10.0.0.499/NVIDIA_app_beta_v10.0.0.499.exe",
+    "https://updates.safing.io/latest/windows_amd64/packages/portmaster-installer.exe",
+    "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe"
+)
 
-# Download installers
 foreach ($link in $links) {
     if (!$link.EndsWith(".exe")) { 
         $request = Invoke-WebRequest -Uri $link -MaximumRedirection 0 -ErrorAction SilentlyContinue
-        $regex = '<a href="([^"]+)">([^<]+)</a>'
-        $link = [regex]::Match($request, $regex).Groups[1].Value
+        $link = $request.Headers.Location
     }
 
     $installerPath = Join-Path $env:HOMEPATH\downloads (Split-Path $link -Leaf).Replace("%20", " ")
